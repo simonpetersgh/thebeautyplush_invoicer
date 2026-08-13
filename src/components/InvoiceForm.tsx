fac6eae5673142251d5ac6a2beea63f981871d6f
Simon Peters <159218431+simonpetersgh@users.noreply.github.com>
2026-08-13 15:13:22 +0000

    Update InvoiceForm.tsx

--- src/components/InvoiceForm.tsx
+++ src/components/InvoiceForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { InvoiceData, LineItem } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
